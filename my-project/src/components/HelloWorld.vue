f3bb543273f7a70650d93f793ae5cdd785534955
wenda <471780268@qq.com>
2021-03-01 14:31:23 +0800

    vue练习

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button
+      v-for="(item, index) in girls"
+      :key="index"
+      @click="selectGirlFun(index)"
+    >
+      {{item}}
+    </button>
+
+  <div v-if="selectGirls == ''">
+    您还未进行选择
+  </div>
+  <div v-if="selectGirls !== ''">
+    恭喜您，选择了我们的【{{selectGirls}}】为您服务！
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup() {
+
+    const girls = ref(["小红", "小黄", "小白"]);
+    const selectGirls = ref("");
+    const selectGirlFun = (index: number) => {
+      selectGirls.value = girls.value[index];
+    };
+
+    return {
+      girls,
+      selectGirls,
+      selectGirlFun,
+    };
+
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ my-project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="欢迎来到红浪漫洗浴中心"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
